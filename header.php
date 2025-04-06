81fdecc8e94fcd74f62aa799be8db3338b005fa4
erisaramadanii <erisa.ramadani@student.uni-pr.edu>
2025-04-06 15:47:25 +0200

    Update header.php

--- header.php
+++ header.php
@@ -1,1 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Aplikacioni PHP</title>
+    <link rel="stylesheet" href="assets/style.css">
+</head>
+<body>
+<h1>Mirë se vini!</h1>
 
