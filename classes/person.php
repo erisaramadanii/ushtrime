f882b42b92858bcc548609cd9db8be420ebc365c
erisaramadanii <erisa.ramadani@student.uni-pr.edu>
2025-04-06 15:49:31 +0200

    Update person.php

--- classes/person.php
+++ classes/person.php
@@ -1,1 +1,32 @@
+<?php
+class Person {
+    private $emri;
+    private $mosha;
+
+    public function __construct($emri, $mosha) {
+        $this->emri = $emri;
+        $this->mosha = $mosha;
+    }
+
+    public function getInfo() {
+        return "Emri: " . $this->emri . " | Mosha: " . $this->mosha;
+    }
+
+    public function getEmri() {
+        return $this->emri;
+    }
+
+    public function setEmri($emri) {
+        $this->emri = $emri;
+    }
+
+    public function getMosha() {
+        return $this->mosha;
+    }
+
+    public function setMosha($mosha) {
+        $this->mosha = $mosha;
+    }
+}
+?>
 
