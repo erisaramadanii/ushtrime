ee577df9ce3c673eb506d390907fd6072e728a63
erisaramadanii <erisa.ramadani@student.uni-pr.edu>
2025-04-06 15:48:06 +0200

    Update menu.php

--- menu.php
+++ menu.php
@@ -1,1 +1,9 @@
+<nav>
+    <ul>
+        <li><a href="index.php">Ballina</a></li>
+        <li><a href="index.php?page=about">Rreth nesh</a></li>
+        <li><a href="index.php?page=contact">Kontakt</a></li>
+    </ul>
+</nav>
+<hr>
 
