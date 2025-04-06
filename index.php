738079bab89ccfe84a06efb430cb18b4fba2e0c3
erisaramadanii <erisa.ramadani@student.uni-pr.edu>
2025-04-06 15:47:06 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,21 @@
+<?php
+include 'header.php';
+include 'menu.php';
+
+$page = isset($_GET['page']) ? $_GET['page'] : 'home';
+
+switch ($page) {
+    case 'about':
+        include 'about.php';
+        break;
+    case 'contact':
+        include 'contact.php';
+        break;
+    default:
+        include 'home.php';
+        break;
+}
+
+include 'footer.php';
+?>
 
